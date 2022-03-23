9165781c5eebea66ccd409daed02ea45a2fc06a6
N Bizzell <bizzbizzell@gmail.com>
2022-03-23 09:38:22 +0000

    session progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import './App.css';
-import View from './view'
+import View from './view';
+import Add from './Add';
 
 function App(){
 
+  const [todos, changeTodos] = useState([
+    { id: 1, description: "make static data", complete: false },
+    { id: 2, description: "make dynamic data", complete: false }
+  ])
+
+  const updateList =(id, description, complete) =>{
+    console.log(complete)
+    const listItem = {id, description, complete}
+    changeTodos((prevState) =>[...prevState, listItem] )
+  }
+
     return (
         <Container>
-          <View />
+          <Add updateList={(id, description, completed) => 
+            updateList(id, description, completed)}
+          />
+          <View todos = {todos}/>
         </Container>
     );
 
